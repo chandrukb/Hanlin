337faaee97c55d3e0cbd52eece971d4095487b42
Palanisamy Manickam <palanisamymanickam89@gmail.com>
2018-01-27 00:36:09 +0530

    Roundedimageview created and profile page ui update

---
+++ Hanlin/Hanlin/View/HNRoundedImageView.h
@@ -0,0 +1,16 @@
+//
+//  HNRoundeImageView.h
+//  Hanlin
+//
+//  Created by Selvam M on 1/27/18.
+//  Copyright © 2018 Balachandran Kaliyamoorthy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+IB_DESIGNABLE
+@interface HNRoundedImageView : UIImageView
+
+@property (nonatomic) IBInspectable CGFloat cornerRadious;
+@property (nonatomic) IBInspectable BOOL masksToBounds;
+
+@end
---
+++ Hanlin/Hanlin/View/HNRoundedImageView.m
@@ -0,0 +1,21 @@
+//
+//  HNRoundeImageView.m
+//  Hanlin
+//
+//  Created by Selvam M on 1/27/18.
+//  Copyright © 2018 Balachandran Kaliyamoorthy. All rights reserved.
+//
+
+#import "HNRoundedImageView.h"
+
+@implementation HNRoundedImageView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
