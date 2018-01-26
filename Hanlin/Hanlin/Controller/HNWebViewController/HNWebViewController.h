5a6a0e794cd4dddf4bf9064a31c93acfab50eb27
Palanisamy Manickam <palanisamymanickam89@gmail.com>
2018-01-26 21:59:32 +0530

    Webviewcontroller added

---
+++ Hanlin/Hanlin/Controller/HNWebViewController/HNWebViewController.h
@@ -0,0 +1,13 @@
+//
+//  HNWebViewController.h
+//  Hanlin
+//
+//  Created by Selvam M on 1/26/18.
+//  Copyright © 2018 Balachandran Kaliyamoorthy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HNWebViewController : UIViewController
+
+@end
