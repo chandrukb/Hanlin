c4b1870d83bdc66e0f61259e41c65816e331c59f
Palanisamy Manickam <palanisamymanickam89@gmail.com>
2018-01-27 01:10:42 +0530

    Contacts screen added

---
+++ Hanlin/Hanlin/Controllers/HNContactUSVC.h
@@ -0,0 +1,13 @@
+//
+//  HNContactUSVC.h
+//  Hanlin
+//
+//  Created by Selvam M on 1/27/18.
+//  Copyright © 2018 Balachandran Kaliyamoorthy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HNContactUSVC : UIViewController
+
+@end
---
+++ Hanlin/Hanlin/Controllers/HNContactUSVC.m
@@ -0,0 +1,37 @@
+//
+//  HNContactUSVC.m
+//  Hanlin
+//
+//  Created by Selvam M on 1/27/18.
+//  Copyright © 2018 Balachandran Kaliyamoorthy. All rights reserved.
+//
+
+#import "HNContactUSVC.h"
+
+@interface HNContactUSVC ()
+
+@end
+
+@implementation HNContactUSVC
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
