58ab1e6cca259a32f69b85fefb66adcc26a9ac4c
Palanisamy Manickam <palanisamymanickam89@gmail.com>
2018-01-27 21:57:04 +0530

    notification screen ui work

---
+++ Hanlin/Hanlin/Controllers/HNNotificationVC.h
@@ -0,0 +1,13 @@
+//
+//  HNNotificationVC.h
+//  Hanlin
+//
+//  Created by Selvam M on 1/27/18.
+//  Copyright © 2018 BALACHANDRAN K. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HNNotificationVC : UIViewController
+
+@end
---
+++ Hanlin/Hanlin/Controllers/HNNotificationVC.m
@@ -0,0 +1,37 @@
+//
+//  HNNotificationVC.m
+//  Hanlin
+//
+//  Created by Selvam M on 1/27/18.
+//  Copyright © 2018 BALACHANDRAN K. All rights reserved.
+//
+
+#import "HNNotificationVC.h"
+
+@interface HNNotificationVC ()
+
+@end
+
+@implementation HNNotificationVC
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
